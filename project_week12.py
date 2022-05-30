1b45ff6b295038efa55cdb1bcab9a12b3fafa097
Zach <Manklr90@yahoo.com>
2022-05-30 11:17:37 -0500

    Signed-off-by: Zach <Manklr90@yahoo.com>

---
+++ project_week12.py
@@ -0,0 +1,36 @@
+import requests
+
+
+def weather_data(query):
+   
+   api_key = "d5f593af87d9a2662dfacde0cb06fb5f"
+   
+   base_url = "http://api.openweathermap.org/data/2.5/weather?"
+   complete_url = base_url + "appid=" + api_key + "&" + query
+   
+   res=requests.get(complete_url);
+   return res.json();
+
+
+def display_results(weathers,city):
+   print("{}'s temperature: {}°C ".format(city,weathers['main']['temp']))
+   print("Wind speed: {} m/s".format(weathers['wind']['speed']))
+   print("Description: {}".format(weathers['weather'][0]['description']))
+   print("Weather: {}".format(weathers['weather'][0]['main']))
+
+
+def main():
+   
+   city=input('Enter the city:')
+   print()
+   
+   try:
+      query='q='+city;
+      w_data=weather_data(query);
+      display_results(w_data, city)
+      print()
+   except:
+      print('City name not found')
+
+if __name__=='__main__':
+   main()
